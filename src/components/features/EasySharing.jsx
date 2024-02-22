23d34c44ff9fd6b60c9e20b5036df16ece7336e3
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-02-22 09:41:28 +0000

    Removed un-needed files

--- src/components/features/EasySharing.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EasySharing = () => {
-  return <div>EasySharing</div>;
-};
-
-export default EasySharing;
--- src/components/features/SimpleBookmarking.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SimpleBookmarking = () => {
-  return <div>SimpleBookmarking</div>;
-};
-
-export default SimpleBookmarking;
