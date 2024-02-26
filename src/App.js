6b7005baf1d8814ba38e98806c3f701cd0e0bb48
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-02-26 11:09:29 +0000

    Added the mobile nav view

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 import Contacts from "./components/contacts/Contacts";
 import Extensions from "./components/extensions/Extensions";
@@ -5,19 +6,23 @@ import FrequentlyAskedQuestions from "./components/faq/FrequentlyAskedQuestions"
 import Features from "./components/features/Features";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import MobileNav from "./mobile-nav/MobileNav";
 
 function App() {
+  const [showMobileNav, setShoMobileNav] = useState(false);
   return (
-    <>
-      <div className={styles.wrapper}>
-        <Header />
-        <Features />
-        <Extensions />
-        <FrequentlyAskedQuestions />
-      </div>
+    <div className={styles.wrapper}>
+      {showMobileNav && <MobileNav setMobileNavState={setShoMobileNav} />}
+      <Header
+        showMobileNav={showMobileNav}
+        setMobileNavState={setShoMobileNav}
+      />
+      <Features />
+      <Extensions />
+      <FrequentlyAskedQuestions />
       <Contacts />
       <Footer />
-    </>
+    </div>
   );
 }
 
